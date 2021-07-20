bc70c5721a70cc02777a0e4283d8fdfee43c82c4
Madilynalisa <87683135+Madilynalisa@users.noreply.github.com>
2021-07-20 15:46:28 +0800

    版本1.01

--- CV/videoReceive.py
+++ CV/videoReceive.py
@@ -34,39 +34,14 @@ command = ['ffmpeg',
 
 # 读流函数
 def Video():
-    vid = cv2.VideoCapture(camera_path)
+        vid = cv2.VideoCapture(camera_path)
     if not vid.isOpened():
         raise IOError("could't open webcamera or video")
     while (vid.isOpened()):
         ret, frame = cap.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('frame', gray)
+        cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
-
-def push_stream(left_x, left_y, right_x, right_y):
-    # 管道配置
-    while True:
-        if len(command) > 0:
-            p = sp.Popen(command, stdin=sp.PIPE)
-            break
-
-    while True:
-        if not frame_queue.empty():
-            frame = frame_queue.get()
-            if frame is not None:
-                # 判断frame是否为NoneType
-                image = cv2.resize(frame[int(left_x):int(right_x)][int(left_y):int(right_y)], (width, height))
-                p.stdin.write(image.tostring())
-
-
-def run(left_x, left_y, right_x, right_y):
-    thread_video = threading.Thread(target=Video, )
-    thread_push = threading.Thread(target=push_stream, args=(left_x, left_y, right_x, right_y,))
-    thread_video.start()
-    thread_push.start()
-
 
 if __name__ == "__main__":
     Video()
